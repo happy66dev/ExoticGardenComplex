c2fdcd2de9320f2ba5dabaf379c2304fa10889f8
happy <k666kkk666k@163.com>
2026-06-20 00:26:43 +0800

    chore: 同步DishGenerator用户修改，版本升至1.0.32喵~

--- src/main/java/io/github/thebusybiscuit/exoticgarden/ExoticCommand.java
+++ src/main/java/io/github/thebusybiscuit/exoticgarden/ExoticCommand.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class ExoticCommand implements CommandExecutor {
 
     // 当前插件版本号，每次 commit 时手动更新喵
-    public static final String PLUGIN_VERSION = "1.0.31";
+    public static final String PLUGIN_VERSION = "1.0.32";
 
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         // /exotic version 喵~
